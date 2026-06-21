ce2dd3d65b94a441f6c8050fddbbc628884ed4ff
jesutofunmi <jesutofunmiisrael30@gmail.com>
2026-06-21 21:17:42 +0100

    fix

---
+++ Model/Cardmodel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema(
+  {
+    card_type: {
+      type: String,
+      required: true,
+    },
+    card_code: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Card", cardSchema);
